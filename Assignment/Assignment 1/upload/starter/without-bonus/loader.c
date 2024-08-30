b6c5621d746033bad0ff2b8f784343a0f2225aef
VarunGolcha20 <162430064+VarunGolcha20@users.noreply.github.com>
2024-08-30 15:19:40 +0530

    Update loader.c

--- Assignment/Assignment 1/upload/starter/without-bonus/loader.c
+++ Assignment/Assignment 1/upload/starter/without-bonus/loader.c
@@ -8,7 +8,8 @@ int fd;
  * release memory and other cleanups
  */
 void loader_cleanup() {
-  
+  if(ehdr!=NULL){free(ehdr);}
+  if(fd>=0){free(fd);}
 }
 
 /*
@@ -24,19 +25,20 @@ void load_and_run_elf(char** exe) {
     exit(1);
   }
 
-  Elf32_Ehdr ehdr;
+  ehdr=(Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
   if(read(fd,&ehdr,sizeof(ehdr))!=sizeof(ehdr)){
     printf("ERROR 2\n");
     exit(1);
   }
 
-  unsigned int phoff=ehdr.e_phoff;
-  unsigned short phentsize=ehdr.e_phentsize;
-  unsigned int phnum=ehdr.e_phnum;
-  unsigned int entry=ehdr.e_entry;
+  unsigned int phoff=ehdr->e_phoff;
+  unsigned short phentsize=ehdr->e_phentsize;
+  unsigned int phnum=ehdr->e_phnum;
+  unsigned int entry=ehdr->e_entry;
 
   // 2. Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
   Elf32_Phdr Phdr;
+  int flag=0;
   for(int i=0;i<phnum;i++){
     lseek(fd,phoff+phentsize*i,SEEK_SET);
     if(read(fd,&Phdr,sizeof(Phdr))!=sizeof(Phdr)){
@@ -47,10 +49,14 @@ void load_and_run_elf(char** exe) {
       continue;
     }
     else if(Phdr.p_vaddr<=entry && Phdr.p_vaddr+Phdr.p_memsz>=entry){
+      flag=1;
       break;
     }
   }
-
+  if(!flag){
+    printf("ERROR 4\n");
+    exit(1);
+  }
   int realoff=entry-Phdr.p_vaddr;
 
   // 3. Allocate memory of the size "p_memsz" using mmap function and then copy the segment content
@@ -59,7 +65,7 @@ void load_and_run_elf(char** exe) {
   lseek(fd, Phdr.p_offset, SEEK_SET);
 
   if (read(fd, virtual_mem, Phdr.p_memsz) != Phdr.p_memsz){
-    perror("ERROR 4\n");
+    perror("ERROR 5\n");
     exit(1);
   }
 
@@ -72,6 +78,7 @@ void load_and_run_elf(char** exe) {
   // 6. Call the "_start" method and print the value returned from the "_start"
   int result = _start();
   printf("User _start return value = %d\n",result);
+  munmap(virtual_mem,Phdr.p_memsz);
 }
 
 int main(int argc, char** argv) 
