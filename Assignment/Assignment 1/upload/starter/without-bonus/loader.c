62254d64a3a3e302def8563492dba18377993841
RamanujanGopalan <160248204+RamanujanGopalan@users.noreply.github.com>
2024-08-30 16:35:30 +0530

    Update loader.c

--- Assignment/Assignment 1/upload/starter/without-bonus/loader.c
+++ Assignment/Assignment 1/upload/starter/without-bonus/loader.c
@@ -8,8 +8,11 @@ int fd;
  * release memory and other cleanups
  */
 void loader_cleanup() {
+  printf("GHJ\n");
   if(ehdr!=NULL){free(ehdr);}
+  printf("JHBFD\n");
   if(fd>=0){free(fd);}
+  printf("JBKFB\n");
 }
 
 /*
@@ -26,7 +29,7 @@ void load_and_run_elf(char** exe) {
   }
 
   ehdr=(Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
-  if(read(fd,&ehdr,sizeof(ehdr))!=sizeof(ehdr)){
+  if(read(fd,ehdr,sizeof(Elf32_Ehdr))!=sizeof(Elf32_Ehdr)){
     printf("ERROR 2\n");
     exit(1);
   }
@@ -36,19 +39,19 @@ void load_and_run_elf(char** exe) {
   unsigned int phnum=ehdr->e_phnum;
   unsigned int entry=ehdr->e_entry;
 
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
-  Elf32_Phdr Phdr;
+  // 2. Iterate through the phdr table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
+  phdr = (Elf32_Phdr *)malloc(sizeof(Elf32_Phdr));
   int flag=0;
   for(int i=0;i<phnum;i++){
     lseek(fd,phoff+phentsize*i,SEEK_SET);
-    if(read(fd,&Phdr,sizeof(Phdr))!=sizeof(Phdr)){
+    if(read(fd,phdr,sizeof(Elf32_Phdr))!=sizeof(Elf32_Phdr)){
       printf("ERROR 3\n");
       exit(1);
     }
-    if(Phdr.p_type!=1){
+    if(phdr->p_type!=1){
       continue;
     }
-    else if(Phdr.p_vaddr<=entry && Phdr.p_vaddr+Phdr.p_memsz>=entry){
+    else if(phdr->p_vaddr<=entry && phdr->p_vaddr+phdr->p_memsz>=entry){
       flag=1;
       break;
     }
@@ -57,14 +60,14 @@ void load_and_run_elf(char** exe) {
     printf("ERROR 4\n");
     exit(1);
   }
-  int realoff=entry-Phdr.p_vaddr;
+  int realoff=entry-phdr->p_vaddr;
 
   // 3. Allocate memory of the size "p_memsz" using mmap function and then copy the segment content
-  void* virtual_mem=mmap(NULL,Phdr.p_memsz,PROT_READ|PROT_EXEC|PROT_WRITE,MAP_PRIVATE,fd,Phdr.p_offset);
+  void* virtual_mem=mmap(NULL,phdr->p_memsz,PROT_READ|PROT_EXEC|PROT_WRITE,MAP_PRIVATE,fd,phdr->p_offset);
   
-  lseek(fd, Phdr.p_offset, SEEK_SET);
+  lseek(fd, phdr->p_offset, SEEK_SET);
 
-  if (read(fd, virtual_mem, Phdr.p_memsz) != Phdr.p_memsz){
+  if (read(fd, virtual_mem, phdr->p_memsz) != phdr->p_memsz){
     perror("ERROR 5\n");
     exit(1);
   }
@@ -78,7 +81,7 @@ void load_and_run_elf(char** exe) {
   // 6. Call the "_start" method and print the value returned from the "_start"
   int result = _start();
   printf("User _start return value = %d\n",result);
-  munmap(virtual_mem,Phdr.p_memsz);
+  munmap(virtual_mem,phdr->p_memsz);
 }
 
 int main(int argc, char** argv) 
