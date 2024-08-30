c600d5660b498479aef5df92159d0ce5a8c5ab84
RamanujanGopalan <160248204+RamanujanGopalan@users.noreply.github.com>
2024-08-30 14:38:26 +0530

    Update loader.c

--- Assignment/Assignment 1/upload/starter/without-bonus/loader.c
+++ Assignment/Assignment 1/upload/starter/without-bonus/loader.c
@@ -15,14 +15,60 @@ void loader_cleanup() {
  * Load and run the ELF executable file
  */
 void load_and_run_elf(char** exe) {
-  fd = open(argv[1], O_RDONLY);
+
+  fd = open(exe, O_RDONLY);
+
   // 1. Load entire binary content into the memory from the ELF file.
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD 
-  //    type that contains the address of the entrypoint method in fib.c
-  // 3. Allocate memory of the size "p_memsz" using mmap function 
-  //    and then copy the segment content
+  if(fd<0){
+    printf("ERROR 1\n");
+    exit(1);
+  }
+
+  Elf32_Ehdr ehdr;
+  if(read(fd,&ehdr,sizeof(ehdr))!=sizeof(ehdr)){
+    printf("ERROR 2\n");
+    exit(1);
+  }
+
+  unsigned int phoff=ehdr.e_phoff;
+  unsigned short phentsize=ehdr.e_phentsize;
+  unsigned int phnum=ehdr.e_phnum;
+  unsigned int entry=ehdr.e_entry;
+
+  // 2. Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
+  Elf32_Phdr Phdr;
+  for(int i=0;i<phnum;i++){
+    lseek(fd,phoff+phentsize*i,SEEK_SET);
+    if(read(fd,&Phdr,sizeof(Phdr))!=sizeof(Phdr)){
+      printf("ERROR 3\n");
+      exit(1);
+    }
+    if(Phdr.p_type!=1){
+      continue;
+    }
+    else if(Phdr.p_vaddr<=entry && Phdr.p_vaddr+Phdr.p_memsz>=entry){
+      break;
+    }
+  }
+
+  int realoff=entry-Phdr.p_vaddr;
+
+  // 3. Allocate memory of the size "p_memsz" using mmap function and then copy the segment content
+  void* virtual_mem=mmap(NULL,Phdr.p_memsz,PROT_READ|PROT_EXEC|PROT_WRITE,MAP_PRIVATE,fd,Phdr.p_offset);
+  
+  lseek(fd, Phdr.p_offset, SEEK_SET);
+
+  if (read(fd, virtual_mem, Phdr.p_memsz) != Phdr.p_memsz){
+    perror("ERROR 4\n");
+    exit(1);
+  }
+
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+  void* real_entry=realoff+virtual_mem;
+
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
+  int (*_start)() = (int (*)()) real_entry;
+
   // 6. Call the "_start" method and print the value returned from the "_start"
   int result = _start();
   printf("User _start return value = %d\n",result);
