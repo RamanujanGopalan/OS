384b979ae7ff6953759c8260dea4e0275421af77
VarunGolcha20 <162430064+VarunGolcha20@users.noreply.github.com>
2024-11-08 13:23:53 +0530

    Update loader.c

--- Assignment/Assignment 4/loader.c
+++ Assignment/Assignment 4/loader.c
@@ -1,4 +1,76 @@
+#include "loader.h"
 
+Elf32_Ehdr *ehdr = NULL;  
+Elf32_Phdr *phdr = NULL; 
+int fd = -1;             
+int count = 0;           
+int fragmentation = 0; 
+
+void loader_cleanup() {
+    if (ehdr != NULL) {
+        free(ehdr);
+        ehdr = NULL;
+    }
+    if (phdr != NULL) {
+        free(phdr);
+        phdr = NULL;
+    }
+    if (fd >= 0) {
+        close(fd);
+        fd = -1;
+    }
+}
+void my_handler(int signo, siginfo_t *info, void *context) {
+    if (signo == SIGSEGV) {
+        uintptr_t seg_address = (uintptr_t)info->si_addr;
+        for (int i = 0; i < ehdr->e_phnum; i++) {
+            if (phdr[i].p_type == 1){
+                if (seg_address >= phdr[i].p_vaddr && seg_address < phdr[i].p_vaddr + phdr[i].p_memsz) {
+
+                    printf("v adrs %d to %d TYPE %d\n", phdr[i].p_vaddr, phdr[i].p_vaddr+phdr[i].p_memsz, phdr[i].p_type);
+                    printf("seg adrs %d\n", seg_address);
+
+                    off_t file_size = lseek(fd, 0, SEEK_END);
+                    if (file_size == -1) {
+                        perror("lseek failed to determine file size");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    printf("FILE SZ %d\n", file_size);
+
+                    int page_num = (seg_address - phdr[i].p_vaddr) / 4096;
+                    int page_start = page_num * 4096;
+                    void *page = mmap((void *)(phdr[i].p_vaddr + page_num * 4096), 4096,
+                                    PROT_READ | PROT_WRITE | PROT_EXEC,
+                                    MAP_PRIVATE | MAP_FIXED |MAP_ANONYMOUS, -1, 0);
+                    if (page == MAP_FAILED) {
+                        perror("mmap failed for page");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    count++;
+                    int to_read = 4096;
+                    if (phdr[i].p_vaddr + phdr[i].p_memsz - seg_address < 4096){
+                        to_read = phdr[i].p_vaddr + phdr[i].p_memsz - seg_address;
+                        fragmentation += 4096-to_read;
+                    }
+
+                    printf("Page %d = %d\n", page_num,  phdr[i].p_offset);
+                    lseek(fd, phdr[i].p_offset + page_num * 4096, SEEK_SET);
+                    int bytes_read = read(fd, page, to_read);
+                    printf("READ %d == %d\n", bytes_read, to_read);
+
+                    if (bytes_read == -1) {
+                        perror("Error reading segment page");
+                        exit(EXIT_FAILURE);
+                    }
+                    
+                    break;
+                }
+            }
+        }
+    }
+}
 void load_and_run_elf(char** exe) {
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
